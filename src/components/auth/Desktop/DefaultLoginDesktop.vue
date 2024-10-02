d079d6a316955e26948e012b09264ed0c40cccff
JSkahl <josekahl333@gmail.com>
2024-10-02 06:42:22 -0300

    chore: alterações no componente login

--- src/components/auth/Desktop/DefaultLoginDesktop.vue
+++ src/components/auth/Desktop/DefaultLoginDesktop.vue
@@ -5,21 +5,29 @@
     <main>
         <div class="form">
             <img src="/public/logo.png" alt="">
-        <h1>Bem-vindo de volta!</h1>
-        <h3>Por favor, insira suas informações</h3>
+            <h1>Bem-vindo de volta!</h1>
+            <p>Por favor, insira suas informações</p>
 
-        <form>
-            <input class="input" type="email" placeholder="Email">
-            <input class="input" type="password" placeholder="Senha">
-            <input class="checkbox" type="checkbox" name="forget">
-            <label for="forget">Lembrar conta</label>
-            <p>Esqueceu a senha?</p>
-            <button>Login</button>
-        </form>
+            <form>
+                <div class="inputs">
+                    <input type="email" placeholder="Email">
+                    <input type="password" placeholder="Senha">
+                </div>
+                <div class="additionals">
+                    <span>
+                    <div class="checkbox">
+                        <input type="checkbox">
+                    </div>
+                    <label>Lembrar conta</label>
+                    </span>
+                    <p>Esqueceu a senha?</p>
+                </div>
+                <button>Login</button>
+            </form>
 
-        <div class="registrar">
-            <p>Não tem uma conta? Registre-se</p>
-        </div>
+            <div class="registrar">
+                <p>Não tem uma conta? Registre-se</p>
+            </div>
         </div>
     </main>
 </template>
@@ -27,7 +35,7 @@
 <style scoped>
 
 main {
-    width: 35vw;
+    width: 30vw;
     height: 96vh;
     box-shadow: 0px 0px 10px black;
     border-radius: 20px;
@@ -36,13 +44,62 @@ main {
 .form {
     display: flex;
     flex-direction: column;
-    text-align: center;
+    align-items: center;
+    justify-self: center;
     padding: 2vw;
 }
 
 .form > img {
     margin: auto;
     width: 6vw;
+}
+
+.inputs {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.inputs input {
+    margin-bottom: 2vh;
+    height: 4vh;
+    width: 20vw;
+    padding: 0 0 0 1vw;
+    border: 0;
+    border-bottom: 2px rgb(52, 52, 52) solid;
+}
+
+.additionals {
+    display: flex;
+    justify-content: space-between;
+    padding: 1vh 0 0 0;
+}
+
+.checkbox {
+    height: 2.2vh;
+    width: 1vw;
+    padding: 0;
+    margin: 0;
+    border: 2px rgb(52, 52, 52) solid;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.checkbox input {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    opacity: 0;
+}
+
+.checkbox:checked .checkbox {
+    background-color: aqua;
+}
+
+
+.additionals p {
+    margin: 0;
 }
 
 </style>
