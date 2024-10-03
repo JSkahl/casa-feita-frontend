cd4cc906ab8f653c6641ad3960f6adb34f779da3
JSkahl <josekahl333@gmail.com>
2024-10-03 15:24:38 -0300

    feat: creating sign up desktop basic screen

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,1 +1,10 @@
-    
+<script setup>
+import DefaultSignUpDesktop from '@/components/auth/Desktop/DefaultSignUpDesktop.vue';
+</script>
+
+<template>
+    <DefaultSignUpDesktop />
+</template>
+
+<style scoped>
+</style>
