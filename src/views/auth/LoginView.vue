e921bedda8cacdffbd9cb43d71156fff15066ed3
JSkahl <josekahl333@gmail.com>
2024-10-03 15:08:28 -0300

    feat: adding responsive logic in login screen

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,8 +1,20 @@
 <script setup>
+import { useScreen } from '@/composables/screen';
+
+const { isMobile } = useScreen()
+
 import DefaultLoginMobile from '@/components/auth/Mobile/DefaultLoginMobile.vue';
+import DefaultLoginDesktop from '@/components/auth/Desktop/DefaultLoginDesktop.vue';
 </script>
 
 <template>
-    <DefaultLoginMobile />
+    <div v-if="isMobile" class="mobile">
+        <DefaultLoginMobile />
+    </div>
+
+    <div v-else class="desktop">
+        <DefaultLoginDesktop />
+    </div>
+
     <RouterView />
 </template>
