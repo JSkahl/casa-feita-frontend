1c4e54675ae518afd784a368e3b2c9f1801d3b34
ViniciusPalharess <depaulapalharesv@gmail.com>
2024-10-22 16:54:21 -0300

    chore: responsividade na header

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2nmb822chd8"
+    "revision": "0.pe6g2gcia3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
