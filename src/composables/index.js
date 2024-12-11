36971a2030b802546099fb540deb82f616c6ea99
JSkahl <josekahl333@gmail.com>
2024-12-11 14:58:07 -0300

    refactor(composables): creating composables index

---
+++ src/composables/index.js
@@ -0,0 +1,9 @@
+import { useLayout } from "@/composables/useLayout";
+import { useMonitor, useOrientation, useScreen } from "@/composables/screen";
+
+export { 
+    useLayout,
+    useMonitor, 
+    useOrientation, 
+    useScreen, 
+};
