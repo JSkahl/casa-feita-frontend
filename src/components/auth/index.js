d9ff5ae6d09ccf133b3faa0aec61344c1bde5179
JSkahl <josekahl333@gmail.com>
2024-12-11 14:57:24 -0300

    chore(auth): fixing problem in auth component index

---
+++ src/components/auth/index.js
@@ -0,0 +1,15 @@
+import DesktopLogin from '@/components/auth/Desktop/Login';
+import MobileLogin from '@/components/auth/Mobile/Login';
+import DesktopSignUp from '@/components/auth/Desktop/SignUp';
+import MobileSignUp from '@/components/auth/Mobile/SignUp';
+import DefaultLogin from '@/components/auth';
+import DefaultSignUp from '@/components/auth';
+
+export {
+    DesktopLogin,
+    MobileLogin,
+    DesktopSignUp,
+    MobileSignUp,
+    DefaultLogin,
+    DefaultSignUp,
+}
